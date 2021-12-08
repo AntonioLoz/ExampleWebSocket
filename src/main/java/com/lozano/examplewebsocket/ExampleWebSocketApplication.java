2f2f77fa259ae0c8393b21ced7b9fd9561618dc7
LAPTOP-E09VQ1LE\Anton <calerolozanoantonio@gmail.com>
2021-12-08 21:58:51 +0100

    First commit

---
+++ src/main/java/com/lozano/examplewebsocket/ExampleWebSocketApplication.java
@@ -0,0 +1,13 @@
+package com.lozano.examplewebsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleWebSocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleWebSocketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lozano/examplewebsocket/ExampleWebSocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lozano.examplewebsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleWebSocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
